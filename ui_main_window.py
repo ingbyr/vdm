8a2549d92f4c8b0633d3d2cb0a38c2025c3d0cd1
InG_byr <zwkv587@gmail.com>
2016-05-19 23:47:26 +0800

    edit: separate the ui and logic
    Former-commit-id: 9006b6b55ac591df454b30a95d7401c9ffb99cd7

---
+++ ui_main_window.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.6
+#
+# WARNING! All changes made in this file will be lost!
+
