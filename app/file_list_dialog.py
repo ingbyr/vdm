38b81dfc17229d617f385128e8100d433dc75192
ingbyr <ingbyr.com@gmail,com>
2017-07-12 10:57:09 +0800

    get the video info
    Former-commit-id: b6bcbae0c83ffeb6fd1c915bde3fcaea1125af93 [formerly 27cfd25bd66530745577cfaf57e282fe607c3684]
    Former-commit-id: f34b14c19ca6b0cb6853b3479f957de63d9fb42b

--- app/file_list_dialog.py
+++ app/file_list_dialog.py
@@ -7,10 +7,11 @@ website: www.ingbyr.com
 """
 import os
 
-from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QDialog, QListWidgetItem, QMessageBox
 from PyQt5.uic import loadUi
 
-from app.youget_helper import GetVideoInfoThread
+from app import log
+from app.youget_helper import GetMediaInfoThread, get_itag
 
 
 class FileListDialog(QDialog):
@@ -21,12 +22,29 @@ class FileListDialog(QDialog):
         self.init_ui()
 
     def init_ui(self):
-        self.dialog.text_files_list.setPlaceholderText("Loading...")
+        default_item = QListWidgetItem(self.tr("Loading..."))
+        self.dialog.list_widget.addItem(default_item)
         self.dialog.show()
 
-        self.m_thread = GetVideoInfoThread("-i", self.url)
-        self.m_thread.finish_signal.connect(self.show_info)
-        self.m_thread.start()
+        self.info_thread = GetMediaInfoThread("-i", self.url)
+        self.info_thread.finish_signal.connect(self.show_info)
+        self.info_thread.start()
 
     def show_info(self, result):
-        self.dialog.text_files_list.setText(result)
+        self.dialog.list_widget.clear()
+        for op in result:
+            item = QListWidgetItem(self.tr(op))
+            self.dialog.list_widget.addItem(item)
+        self.dialog.list_widget.itemClicked.connect(self.start_download)
+
+    def start_download(self, item):
+        self.show_msg(item.text())
+        tag = get_itag(item.text())
+        print(tag)
+
+    def show_msg(self, text, title="Debug"):
+        self.msg = QMessageBox()
+        self.msg.setWindowTitle(title)
+        self.msg.setText(text)
+        self.msg.setStandardButtons(QMessageBox.Ok)
+        self.msg.show()
