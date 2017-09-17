d252a3309d09dfe3444d18d80430a36c4dcb578d
ingbyr <ingbyr@outlook.com>
2017-09-17 22:40:29 +0800

    show media list
    Former-commit-id: ee595c704dc064c2063c86a7fae3b2dc64245b78 [formerly 22dd4dfe645ecc6aa322c41e6668da13fd3b3bcc]
    Former-commit-id: 40bc87ea756e8dafeee7c293b65b5ba88bb6bba9

--- src/main/kotlin/com/ingbyr/guiyouget/Main.kt
+++ src/main/kotlin/com/ingbyr/guiyouget/Main.kt
@@ -2,16 +2,9 @@ package com.ingbyr.guiyouget
 
 import com.ingbyr.guiyouget.views.MainView
 import javafx.application.Application
-import javafx.stage.Stage
-import javafx.stage.StageStyle
 import tornadofx.*
 
-class Main : App(MainView::class) {
-    override fun start(stage: Stage) {
-        stage.initStyle(StageStyle.UNDECORATED)
-        super.start(stage)
-    }
-}
+class Main : App(MainView::class)
 
 fun main(args: Array<String>) {
     Application.launch(Main::class.java, *args)
