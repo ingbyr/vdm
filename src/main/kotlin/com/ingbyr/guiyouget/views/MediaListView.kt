d70900b713862f50b267c4967ac000b88b9f9f6c
ingbyr <ingbyr@outlook.com>
2018-05-07 17:38:06 +0800

    edit media list view; add new task view
    Former-commit-id: 957f2d9cb823785537e08ab8dd6b4c4d8acac333 [formerly 2398a3f181ce97b4b2fd2e3c604d6e0e7fe7c1fe]
    Former-commit-id: 399c96eca6a3700361ba247d739b698177223417

--- src/main/kotlin/com/ingbyr/guiyouget/views/MediaListView.kt
+++ src/main/kotlin/com/ingbyr/guiyouget/views/MediaListView.kt
@@ -1,19 +1,11 @@
 package com.ingbyr.guiyouget.views
 
-import com.ingbyr.guiyouget.controllers.MediaListController
-import com.ingbyr.guiyouget.events.DownloadMedia
-import com.ingbyr.guiyouget.events.StopBackgroundTask
-import com.ingbyr.guiyouget.models.CurrentConfig
 import com.jfoenix.controls.JFXListView
-import javafx.application.Platform
 import javafx.scene.control.Label
-import javafx.scene.input.MouseEvent
 import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.Pane
-import javafx.stage.StageStyle
-import org.slf4j.LoggerFactory
 import tornadofx.*
 import java.util.*
+
 
 class MediaListView : View() {
 
@@ -21,93 +13,9 @@ class MediaListView : View() {
         messages = ResourceBundle.getBundle("i18n/MediaListView")
     }
 
-    override val root: AnchorPane by fxml("/fxml/MediaListWindow.fxml")
-    private val logger = LoggerFactory.getLogger(MediaListView::class.java)
+    override val root: AnchorPane by fxml("/fxml/MediaListView.fxml")
 
-    private var xOffset = 0.0
-    private var yOffset = 0.0
-
-    private val paneExit: Pane by fxid()
-    private val paneMinimize: Pane by fxid()
-    private val paneBack: Pane by fxid()
-    private val apBorder: AnchorPane by fxid()
     private val labelTitle: Label by fxid()
-    private val labelDescription: Label by fxid()
-    private val listViewMedia: JFXListView<Label> by fxid()
-    private val controller: MediaListController by inject()
-    private var ccf = params["ccf"] as CurrentConfig
-
-    init {
-        // Window boarder
-        apBorder.setOnMousePressed { event: MouseEvent? ->
-            event?.let {
-                xOffset = event.sceneX
-                yOffset = event.sceneY
-            }
-        }
-
-        apBorder.setOnMouseDragged { event: MouseEvent? ->
-            event?.let {
-                primaryStage.x = event.screenX - xOffset
-                primaryStage.y = event.screenY - yOffset
-            }
-        }
-
-        paneExit.setOnMouseClicked {
-            Platform.exit()
-        }
-
-        paneMinimize.setOnMouseClicked {
-            primaryStage.isIconified = true
-        }
-
-        paneBack.setOnMouseClicked {
-            replaceWith(MainView::class, ViewTransition.Slide(0.3.seconds, ViewTransition.Direction.RIGHT))
-        }
-
-        listViewMedia.setOnMouseClicked {
-            listViewMedia.selectedItem?.let {
-                val formatID = it.text.split(" ")[0]
-                logger.debug("start download ${it.text}, format id is $formatID")
-                ccf.formatID = formatID
-                ProgressView().openWindow(StageStyle.UNDECORATED)
-                fire(DownloadMedia(ccf))
-            }
-        }
-    }
-
-    private fun displayMedia() {
-        // fetch media json and display it
-        runAsync {
-            controller.requestMedia(ccf.engineType, ccf.url, ccf.proxyType, ccf.address, ccf.port)
-        } ui {
-            if (it != null) {
-                controller.engine?.displayMediaList(labelTitle, labelDescription, listViewMedia, it)
-            } else {
-                labelTitle.text = messages["failed"]
-            }
-        }
-
-    }
-
-    private fun resetUI() {
-        labelTitle.text = messages["label.loading"]
-        labelDescription.text = ""
-        listViewMedia.items.clear()
-    }
-
-    override fun onUndock() {
-        /**
-         * Reset UI and clean the background task
-         */
-        resetUI()
-        // clean the thread
-        fire(StopBackgroundTask)
-    }
-
-    override fun onDock() {
-        ccf = params["ccf"] as CurrentConfig
-        resetUI()
-        displayMedia() // fetch media json and display
-    }
+    private val labelDesc: Label by fxid()
+    private val listView: JFXListView<Label> by fxid()
 }
