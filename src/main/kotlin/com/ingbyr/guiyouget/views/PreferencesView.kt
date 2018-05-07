6d8dfc5ac697cd09495446d08576fd48f388ee56
ingbyr <ingbyr@outlook.com>
2018-05-07 16:19:04 +0800

    edit preferences view
    Former-commit-id: 002ebd0e9e8a9a956fcf5f14b9329d4ecbae5a37 [formerly 54468c26e08451ff4c5746b4806656fadabf1c5a]
    Former-commit-id: a5db5c44445d634752b7002df4701f62041d0e05

--- src/main/kotlin/com/ingbyr/guiyouget/views/PreferencesView.kt
+++ src/main/kotlin/com/ingbyr/guiyouget/views/PreferencesView.kt
@@ -1,8 +1,13 @@
 package com.ingbyr.guiyouget.views
 
 import com.jfoenix.controls.JFXTabPane
+import com.jfoenix.controls.JFXTextField
+import com.jfoenix.controls.JFXToggleButton
+import javafx.scene.control.Label
+import javafx.scene.control.ToggleGroup
 import tornadofx.*
 import java.util.*
+
 
 class PreferencesView : View() {
     init {
@@ -10,6 +15,24 @@ class PreferencesView : View() {
     }
 
     override val root: JFXTabPane by fxml("/fxml/PreferencesView.fxml")
+
+    private val btnDownloadDefault: JFXToggleButton by fxid()
+    private val tbYoutubeDL: JFXToggleButton by fxid()
+    private val engines: ToggleGroup by fxid()
+    private val labelYoutbeDLVersion: Label by fxid()
+    private val tbYouGet: JFXToggleButton by fxid()
+    private val labelYouGetVersion: Label by fxid()
+    private val tbYkdl: JFXToggleButton by fxid()
+    private val labelYkdlVersion: Label by fxid()
+    private val tbSocks5: JFXToggleButton by fxid()
+    private val proxy: ToggleGroup by fxid()
+    private val tfSocks5Address: JFXTextField by fxid()
+    private val tfSocks5Port: JFXTextField by fxid()
+    private val tbHTTP: JFXToggleButton by fxid()
+    private val tfHTTPAddress: JFXTextField by fxid()
+    private val tfHTTPPort: JFXTextField by fxid()
+    private val labelStoragePath: Label by fxid()
+    private val labelFfmpegPath: Label by fxid()
 
     init {
 
