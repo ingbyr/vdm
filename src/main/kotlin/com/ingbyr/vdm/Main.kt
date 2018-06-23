190b728f82e879d5905db6f623415685527c9b67
ingbyr <ingbyr@outlook.com>
2018-06-23 19:46:38 +0800

    change zh_CN file name
    Former-commit-id: bfdb950c8b8f9ea4ca203501dd33507746554d40 [formerly c0f175f0b835558d63aed88addfca80ec0018ca9]
    Former-commit-id: ea83b6ce774297ef90b0f269f2dcf90d67c2b2e1

--- src/main/kotlin/com/ingbyr/vdm/Main.kt
+++ src/main/kotlin/com/ingbyr/vdm/Main.kt
@@ -17,7 +17,7 @@ class Main : App(MainView::class) {
     private val availableLanguages = listOf("zh", "en", "hu")
 
     init {
-        // Locale.setDefault(Locale("test", "test"))
+//         Locale.setDefault(Locale("", ""))
         val language = Locale.getDefault().language
         if (language !in availableLanguages) {
             Locale.setDefault(Locale("en", "US"))
