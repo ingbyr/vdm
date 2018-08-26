5315c82d5cf5f1f3d2df2f58c8e53ed42feba911
ingbyr <ingbyr@outlook.com>
2018-08-26 21:33:36 +0800

    restore locale
    Former-commit-id: dd1573e4c50b75c7f619a0eca8ab04f6caaa2bab [formerly 502b5dbe08d2e7351e02257bdbf6f0b4cb0fb239]
    Former-commit-id: 36fc09cb379aab3b65577342878caabd406e991d

--- src/main/kotlin/com/ingbyr/vdm/Main.kt
+++ src/main/kotlin/com/ingbyr/vdm/Main.kt
@@ -17,7 +17,7 @@ class Main : App(MainView::class) {
     private val availableLanguages = listOf("zh", "en", "hu")
 
     init {
-         Locale.setDefault(Locale("en", ""))
+//         Locale.setDefault(Locale("en", ""))
         val language = Locale.getDefault().language
         if (language !in availableLanguages) {
             Locale.setDefault(Locale("en", "US"))
